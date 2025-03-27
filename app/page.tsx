79764d819ef1e9034b90d0420c908a14184eba7b
jstgrowup <deysubham999@gmail.com>
2025-03-27 11:26:11 +0530

    project setup

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="border border-black">
+      <Button>Click me</Button>
+    </div>
+  );
+}
