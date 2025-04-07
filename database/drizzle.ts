0d29dcf6b7d0a01c2ddaa20941926b6067b492d6
jstgrowup <deysubham999@gmail.com>
2025-04-08 00:49:51 +0530

    done  with the db setup

--- database/drizzle.ts
+++ database/drizzle.ts
@@ -1,3 +1,5 @@
 import config from "@/lib/config";
+import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
-export const db = drizzle(config);
+const sql = neon(config.env.databaseUrl!);
+export const db = drizzle({ client: sql });
